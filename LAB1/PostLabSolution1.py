d61ba7afbb6865df07bb2c27bd4edd2e14fdf3b1
Angelo Avengoza <126324548+notavailablenot@users.noreply.github.com>
2024-04-28 07:23:32 +0000

    modified:   LAB1/PostLabSolution1.py

--- LAB1/PostLabSolution1.py
+++ LAB1/PostLabSolution1.py
@@ -1,20 +1,29 @@
-import numpy
-from scipy import stats
+sorted_numbers = [2,4,5,2,3,4,5,2,23,4,7,5,4,43] 
 
-list = []
+def median(numbers):
+    sorted_numbers = sorted(numbers)
+    n = len(sorted_numbers)
+    if n % 2 == 0:
+        return (sorted_numbers[n//2 - 1] + sorted_numbers[n//2]) / 2
+    else:
+        return sorted_numbers[n//2]
 
-print("List Size 9")
+def mode(numbers):
+    frequency = {}
+    for num in numbers:
+        if num in frequency:
+            frequency[num] += 1
+        else:
+            frequency[num] = 1
+    max_frequency = max(frequency.values())
+    mode_list = [key for key, value in frequency.items() if value == max_frequency]
+    return mode_list[0]
 
-for i in range(9):
-    
-    list.append(int(input('Enter Value List: ')))
+def mean(numbers):
+    return sum(numbers) / len(numbers)
 
-print("List: ", list)
+print("Mean: ", mean(sorted_numbers))
+print("Median: ", median(sorted_numbers))
+print("Mode: ", mode(sorted_numbers))
 
-x = numpy.mean(list)
-y = numpy.median(list)
-z = stats.mode(list)
 
-print("Median:", y)
-print("Mode:", z)
-print("Mean:", x)
