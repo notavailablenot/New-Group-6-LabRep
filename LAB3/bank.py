06a1da7451192041cbe57fec3ec118ad2f670aed
lajoson <161935076+lajoson@users.noreply.github.com>
2024-04-30 19:52:18 +0800

    Create bank.py

---
+++ LAB3/bank.py
@@ -0,0 +1,98 @@
+"""
+File: bank.py
+This module defines the Bank class.
+"""
+import pickle
+import random
+from savingsaccount import SavingsAccount
+
+class Bank:
+    def __init__(self, fileName = None):
+        self.accounts = {}
+        self.fileName = fileName
+        if fileName != None:
+            fileObj = open(fileName, 'rb')
+            while True:
+                try:
+                    account = pickle.load(fileObj)
+                    self.add(account)
+                except Exception:
+                    fileObj.close()
+                    break
+
+    def __str__(self):
+        sorted_list = sorted(self.accounts.values(), key=lambda account: account.getName())
+        return "\n".join(map(str, sorted_list))
+
+    def makeKey(self, name, pin):
+        return name + "/" + pin
+
+    def add(self, account):
+        key = self.makeKey(account.getName(), account.getPin())
+        self.accounts[key] = account
+
+    def remove(self, name, pin):
+        key = self.makeKey(name, pin)
+        return self.accounts.pop(key, None)
+
+    def get(self, name, pin):
+        key = self.makeKey(name, pin)
+        return self.accounts.get(key, None)
+
+    def computeInterest(self):
+        total = 0
+        for account in self._accounts.values():
+            total += account.computeInterest()
+        return total
+
+    def getKeys(self):
+        return []
+
+    def save(self, fileName = None):
+        if fileName != None:
+            self.fileName = fileName
+        elif self.fileName == None:
+            return
+        fileObj = open(self.fileName, 'wb')
+        for account in self.accounts.values():
+            pickle.dump(account, fileObj)
+        fileObj.close()
+
+def createBank(numAccounts = 1):
+    names = ("Angela", "Ishi", "Earl", "Mark", "Hannah",
+             "Cyris", "Jillian", "Jr")
+    bank = Bank()
+    upperPin = numAccounts + 1000
+    for pinNumber in range(1000, upperPin):
+        name = random.choice(names)
+        balance = float(random.randint(100, 1000))
+        bank.add(SavingsAccount(name, str(pinNumber), balance))
+    return bank
+
+def testAccount():
+    """Test function for savings account."""
+    account = SavingsAccount("Cyris", "1000", 500.00)
+    print(account)
+    print(account.deposit(100))
+    print("Expect 600:", account.getBalance())
+    print(account.deposit(-50))
+    print("Expect 600:", account.getBalance())
+    print(account.withdraw(100))
+    print("Expect 500:", account.getBalance())
+    print(account.withdraw(-50))
+    print("Expect 500:", account.getBalance())
+    print(account.withdraw(100000))
+    print("Expect 500:", account.getBalance())
+
+def main(number = 10, fileName = None):
+
+    if fileName:
+        bank = Bank(fileName)
+    else:
+        bank = createBank(number)
+    print(bank)
+
+if __name__ == "__main__":
+    main()
+
+   
